eb2fc3f05581dc438ecc6cefe63d866d139364b9
Camilo <camilobr17@gmail.com>
2024-12-23 12:17:52 -0500

    Componente_listo

--- src/js/json.js
+++ src/js/json.js
@@ -176,7 +176,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228141_CF04_DU.pdf',
       },
       {
         icono: 'fas fa-download',
